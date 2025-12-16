fefc0cdeaa2a236aaf88325746f2e187b0258747
daf672 <dieganfer27@gmail.com>
2025-12-17 00:28:23 +0100

    Comienzo DetallesLugarScreen.kt

--- app/src/main/java/com/example/sevilla/ui/DetallesLugarScreen.kt
+++ app/src/main/java/com/example/sevilla/ui/DetallesLugarScreen.kt
@@ -1,8 +1,15 @@
 package com.example.sevilla.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sevilla.model.SevillaItem
 
 @Composable
@@ -10,7 +17,20 @@ fun DetallesLugarScreen(
     lugar: SevillaItem.Lugar?,
     modifier: Modifier = Modifier,
 ) {
+    Column {
+        Image(
+            painter = painterResource(lugar?.imagen ?: 0),
+            contentDescription = null
+        )
 
+        Text(
+            text = lugar?.descripcion ?: "",
+            style = MaterialTheme.typography.displayMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+    }
 }
 
 @Preview
